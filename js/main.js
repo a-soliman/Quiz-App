f1f89a17059bea0379ba75f56f3086fe0264784b
Ahmed Soliman <ahmedanwar@mail.com>
2016-07-13 01:06:17 -0700

    coded statement area to show additional status

--- js/main.js
+++ js/main.js
@@ -78,23 +78,59 @@ function calculateAnswer() {
 	}
 
 	// setting the result holder
-	var resultOutput = document.getElementById('result')
+	var resultCorrect = document.getElementById('resultCorrect')
+	var resultIncorrect = document.getElementById('resultIncorrect')
+	var resultNoAnswer= document.getElementById('resultNoAnswer')
+
 	//creating h2 for the results of correct, incorrect and empty	
 	correctResult = document.createElement('h2')
 	incorrectResult = document.createElement('h2')
 	noAnswerResult = document.createElement('h2')
 
+	//adding class conected to CSS to change the style of each
 	correctResult.className = "answer-correct"
 	incorrectResult.className = "answer-incorrect"
 	noAnswerResult.className = "answer-didntAnswer"
 
+	//attaching the text and the counting to the output
 	correctResult.textContent = 'CORRECT: ' + correct
 	incorrectResult.textContent = 'INCORRECT: ' + incorrect
 	noAnswerResult.textContent = "DIDN'T Answer: " + noAnswer
 
-	resultOutput.appendChild(correctResult)
-	resultOutput.appendChild(incorrectResult)
-	resultOutput.appendChild(noAnswerResult)
-	
+	//appending the h2s to the output div
+	resultCorrect.appendChild(correctResult)
+	resultIncorrect.appendChild(incorrectResult)
+	resultNoAnswer.appendChild(noAnswerResult)
+
+//===============	//aditional result =====================//
+	// getting the output
+	var sideOutput = document.getElementById('resultSide')
+	// creating p element
+	var statement = document.createElement('p')
+
+	sideOutput.appendChild(statement)
+
+	// assifning the messages 
+	var magnificent = "magnificent: you scored " + correct + " / " + questionObj.length
+	var veryWell = "VERY-WELL: you scored " + correct + " / " + questionObj.length
+	var halfWay = "HALFWAY: you scored " + correct + " / " + questionObj.length
+	var fail = "YOU REALLY NEED TO STUDY THIS THING.."
+
+	// the if condition
+	if ( correct == questionObj.length )  {
+		statement.textContent = magnificent
+	}
+
+	else if (correct >= (questionObj.length * 80) /100) {
+		statement.textContent = veryWell
+	}
+
+	else if (correct >= (questionObj.length * 50) /100) {
+		statement.textContent = halfWay
+	}
+
+	else if (correct < questionObj.length / 2 ) {
+		statement.textContent = fail
+	}
 
 }
